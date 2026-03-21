e19a10213c9607da2d292805da568bd25b959e05
Skyler Clark <skclark@redhat.com>
2026-03-21 19:03:24 -0400

    adds bow

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 pub mod generated {
     include!(concat!(env!("OUT_DIR"), "/generated_default_skeleton.rs"));
-    #[cfg(feature = "animations")]
-    include!(concat!(env!("OUT_DIR"), "/Stand.rs"));
+}
+
+// TODO: rewrite this to use the default animations hashmap provided by the other repo
+// make this a macro that concats all of these includes
+#[cfg(feature = "animations")]
+pub mod animations {
+    pub mod stand {
+        include!(concat!(env!("OUT_DIR"), "/Stand.rs"));
+    }
+    pub mod bow {
+        include!(concat!(env!("OUT_DIR"), "/Bow.rs"));
+    }
 }
